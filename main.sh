9762134bcfcd68717a6cdba470e5cb8bc56e24e2
Gabriel (Gabs) Cerioni <80788917+gabrielcerioni@users.noreply.github.com>
2021-11-19 17:54:16 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,9 +10,9 @@ filter_mongodb_debug_messages() {
 
 echo "THE QUERY AND HOST PROCESS STARTS HERE..."
 
-${serviceVariable.SUDO_CMD} docker exec mongoContainer mongo "mongodb://${serviceVariable.admin_user}:${serviceVariable.admin_pass}@${serviceVariable.HOST1}:${serviceVariable.MONGODB_PORT},${serviceVariable.HOST2}:${serviceVariable.MONGODB_PORT},${serviceVariable.HOST3}:${serviceVariable.MONGODB_PORT}/harness?authSource=admin&replicaSet=${serviceVariable.replicaset_name}" --quiet --eval 'db.instance.find({isDeleted: false, infraMappingType: "PHYSICAL_DATA_CENTER_SSH"}, {"instanceInfo.hostName": 1, _id: 0}).toArray()' | filter_mongodb_debug_messages > input.json
+${serviceVariable.SUDO_CMD} docker exec mongoContainer mongo "mongodb://${serviceVariable.admin_user}:${serviceVariable.admin_pass}@${serviceVariable.HOST1}:${serviceVariable.MONGODB_PORT},${serviceVariable.HOST2}:${serviceVariable.MONGODB_PORT},${serviceVariable.HOST3}:${serviceVariable.MONGODB_PORT}/harness?authSource=admin&replicaSet=${serviceVariable.replicaset_name}" --quiet --eval 'db.instance.aggregate([{$lookup: {from: "infrastructureMapping", localField: "infraMappingId", foreignField: "_id", as: "infraMappingStuff"}}, {$unwind: "$infraMappingStuff"},{$lookup: {from: "infrastructureDefinitions", localField: "infraMappingStuff.infrastructureDefinitionId", foreignField: "_id", as: "infraDefinitionsStuff"}}, {$unwind: "$infraDefinitionsStuff"}, {$match:{ $and:[{"isDeleted" : false, infraMappingType: "PHYSICAL_DATA_CENTER_SSH", createdAt: {$gt: 1621382400000}}]}}, {$project: {_id: 0, "instanceInfo.hostName": 1, fakeConcatID: {$concat: [ "$serviceName", "___", "$infraDefinitionsStuff.name", "___", "$instanceInfo.hostName"]}}}, {$group: {_id: "$fakeConcatID", deduppedHostname: {$first: "$instanceInfo.hostName"}}}, {$project: {_id: 0}}]).toArray()' | filter_mongodb_debug_messages > input.json
 
-jq -c '.[].instanceInfo.hostName' input.json | while read i; do
+jq -c '.[].deduppedHostname' input.json | while read i; do
     #echo -n "Checking hostname $i..."
     host_to_validate=$(echo $i | tr -d '"')
     host $host_to_validate >>logger.txt 2>&1;
@@ -31,12 +31,5 @@ Inactive count: `cat inactive.services |wc -l`"
 echo "----------------------------------------------"
 
 echo "##############################################"
-
-echo "This extra part of the script is just to dedup those files into a list."
-echo "Each line is a HOST, but the query is on the SIs Collection. So, there is a 1-N relationship here."
-echo "----------------------------------------------"
-echo -e "DEDUPPED - Active(resolvable) host count: `sort active.services |uniq -u |wc -l`
-DEDUPPED - Inactive count: `sort inactive.services |uniq -u |wc -l`"
-echo "----------------------------------------------"
 
 rm {active.services,inactive.services,logger.txt}
